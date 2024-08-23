28c0465666da4345aa67155e00696c0c6dac3eea
“Swapnil <swapnil.doijad@gmail.com>
2024-08-23 09:43:22 +0200

    general chanages

---
+++ 0773_amplicon_sequence_analysis_by_qiime2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+###############################################################################
+## header
+    pipeline=0773_amplicon_sequence_analysis_by_qiime2
+    source /home/groups/VEO/scripts_for_users/supplementary_scripts/my_functions.sh 
+    echo "STARTED : $pipeline --------------------------------------"
+###############################################################################
+## preparation
+
+    create_directories_structure_1 $wd
+    submit_jobs $wd $pipeline
+    
+    echo "ENDED : $pipeline --------------------------------------"
+###############################################################################
