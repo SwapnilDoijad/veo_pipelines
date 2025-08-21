8de4063ce564541e054d51272edb11d6f9b64eb6
“Swapnil <swapnil.doijad@gmail.com>
2025-08-22 01:00:02 +0200

    Auto update: Fri Aug 22 01:00:02 CEST 2025

---
+++ 0088_annotation_prokaryote_by_pgap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+###############################################################################
+## header
+    pipeline=0088_annotation_prokaryote_by_pgap
+    source /home/groups/VEO/scripts_for_users/supplementary_scripts/my_functions.sh
+    log "STARTED : $pipeline ------------------------"
+###############################################################################
+## step-01: file preparations
+    fasta_dir=$(grep "my_fasta_dir" tmp/parameters/$pipeline.* | awk '{print $2}')
+    ls $fasta_dir/ | sed 's/.fasta//g' > list.$pipeline.txt
+    list=list.$pipeline.txt
+
+    create_directories_structure_1 $wd
+    split_list $wd $list
+    submit_jobs $wd $pipeline
+    
+###############################################################################
+## footer
+    log "ENDED : $pipeline ------------------------"
+###############################################################################
---
+++ 0665_phage_taxonomy_by_gpgcn.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+###############################################################################
+## header
+    pipeline=0665_phage_taxonomy_by_gpgcn
+    source /home/groups/VEO/scripts_for_users/supplementary_scripts/my_functions.sh
+###############################################################################
+    echo "STARTED : $pipeline ---------------------------------"
+###############################################################################
+## step-01: file and directory preparation
+    fasta_directory=$( grep my_fasta_dir $parameters | awk '{print $2}' )
+    ls $fasta_directory | sed 's/\.fasta$//' > list.$pipeline.txt
+    list=list.$pipeline.txt
+
+    create_directories_structure_1 $wd
+    split_list $wd $list
+    submit_jobs $wd $pipeline
+###############################################################################
+    echo "FINISHED : $pipeline ---------------------------------"
+###############################################################################
