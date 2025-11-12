5cc50d75f972b1677f233d1f1c9a93bc31adbdd8
Swapnil Doijad <swapnil.doijad@gmail.com>
2025-11-12 01:00:02 +0100

    Auto update: Wed Nov 12 01:00:02 CET 2025

--- 0049_contig_mapping_and_genomeReorganising_by_mauve.sh
+++ 0049_contig_mapping_and_genomeReorganising_by_mauve.sh
@@ -3,16 +3,25 @@
     pipeline=0049_contig_mapping_and_genomeReorganising_by_mauve
     source /home/groups/VEO/scripts_for_users/supplementary_scripts/my_functions.sh
 ###############################################################################
-    echo "STARTED : $pipeline -----------------------------------------------------"
+    log "STARTED : $pipeline -----------------------------------------------------"
 ###############################################################################
     grep -v "#" $parameters | sed '/^$/d' > list.$pipeline.txt
     list=list.$pipeline.txt
+    choise=$(grep -w "my_choise" $parameters | awk -F'\t' '{print $2}')
 
     create_directories_structure_1 $wd
-    split_list $wd list.$pipeline.txt
-    submit_jobs $wd $pipeline
-    
-    mkdir $wd/all_fasta
+    if [ $choise == "ordering" ]; then
+        split_list $wd list.$pipeline.txt
+        submit_jobs $wd $pipeline
+
+        mkdir $wd/all_fasta
+    elif [ $choise == "viewing" ]; then
+        cp /home/groups/VEO/scripts_for_users/supplementary_scripts/$pipeline.sbatch \
+        $wd/tmp/sbatch/
+
+        sbatch $wd/tmp/sbatch/$pipeline.sbatch
+    fi
+
 
 ###############################################################################
     log "ENDED : $pipeline ----------------------"
