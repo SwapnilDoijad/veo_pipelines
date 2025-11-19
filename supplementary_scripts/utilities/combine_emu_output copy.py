dabf7941863aafe073502254ff365eec35aad25a
Swapnil Doijad <swapnil.doijad@gmail.com>
2025-11-19 01:00:02 +0100

    Auto update: Wed Nov 19 01:00:02 CET 2025

--- supplementary_scripts/utilities/combine_emu_output copy.py
+++ supplementary_scripts/utilities/combine_emu_output copy.py
@@ -11,34 +11,34 @@ def combine_abundance_per_file(input_dir, output_file):
         df = pd.read_csv(file, delim_whitespace=True, error_bad_lines=False, warn_bad_lines=True)
         lower_cols = {col.lower(): col for col in df.columns}
 
-        species_col = lower_cols['species']
+        lineage_col = lower_cols['lineage']
         abundance_col = lower_cols['abundance']
 
-        df[species_col] = df[species_col].astype(str).str.strip().str.rstrip(';')
+        df[lineage_col] = df[lineage_col].astype(str).str.strip().str.rstrip(';')
 
-        df_subset = df[[species_col, abundance_col]].copy()
+        df_subset = df[[lineage_col, abundance_col]].copy()
         filename = os.path.basename(file)
         sample_id = filename.split('.')[0]
-        df_subset.columns = ['species', sample_id]
+        df_subset.columns = ['lineage', sample_id]
 
-        df_subset['species'] = df_subset['species'].str.strip()
+        df_subset['lineage'] = df_subset['lineage'].str.strip()
 
         if combined_df.empty:
             combined_df = df_subset
         else:
-            combined_df['species'] = combined_df['species'].str.strip()
-            combined_df = pd.merge(combined_df, df_subset, on='species', how='outer')
+            combined_df['lineage'] = combined_df['lineage'].str.strip()
+            combined_df = pd.merge(combined_df, df_subset, on='lineage', how='outer')
 
     combined_df.fillna(0, inplace=True)
-    combined_df['species'] = combined_df['species'].str.rstrip(';')
+    combined_df['lineage'] = combined_df['lineage'].str.rstrip(';')
 
-    abundance_cols = combined_df.columns.difference(['species'])
+    abundance_cols = combined_df.columns.difference(['lineage'])
     combined_df[abundance_cols] = combined_df[abundance_cols].round(3)
 
     combined_df.to_csv(output_file, sep='\t', index=False)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Combine abundance files with species as rows and filenames as columns.')
+    parser = argparse.ArgumentParser(description='Combine abundance files with lineage as rows and filenames as columns.')
     parser.add_argument('-d', '--input-directory', required=True, 
                         help='Input directory containing abundance files (.tsv)')
     parser.add_argument('-o', '--output', required=True, 
