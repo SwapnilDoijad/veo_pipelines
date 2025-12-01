1e17e13da7e8f7c2fc7689d1ac85ccd252e3d1cb
Swapnil Doijad <swapnil.doijad@gmail.com>
2025-12-02 00:00:03 +0100

    Auto update: Tue Dec  2 00:00:02 CET 2025

--- 0009_demultiplexing_by_barbell.sh
+++ 0009_demultiplexing_by_barbell.sh
@@ -7,13 +7,14 @@
 ###############################################################################
 
     fastq_path=$( grep my_fastq_path $parameters | awk '{print $2}' )
-    ls $fastq_path | sed 's/\.fastq//g' | sort | uniq > list.$pipeline.txt
+    ls $fastq_path/*.fastq | awk -F/ '{print $NF}' | sed 's/\.fastq//g' | sort | uniq > list.$pipeline.txt
     list=list.$pipeline.txt
 	
     create_directories_structure_1 $wd
     split_list $wd $list
     submit_jobs $wd $pipeline
 
+    mkdir -p $out_dir/trimmed
 ###############################################################################
     log "FINISHED : $pipeline -----------------------------------------------"
 ###############################################################################
